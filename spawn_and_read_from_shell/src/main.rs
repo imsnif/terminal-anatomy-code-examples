668a149e5f854faddb5be43d2e4c4e10b466d86f
Aram Drevekenin <aram@poor.dev>
2021-10-25 11:01:54 +0200

    style(fmt): fix indentation

--- spawn_and_read_from_shell/src/main.rs
+++ spawn_and_read_from_shell/src/main.rs
@@ -40,14 +40,14 @@ fn main() {
     let stdout_fd = spawn_pty_with_shell(default_shell);
     let mut read_buffer = vec![];
     loop {
-            match read_from_fd(stdout_fd) {
-                        Some(mut read_bytes) => {
-                                        read_buffer.append(&mut read_bytes);
-                                    }
-                        None => {
-                                        println!("{:?}", String::from_utf8(read_buffer).unwrap());
-                                        std::process::exit(0);
-                                    }
-                    }
+        match read_from_fd(stdout_fd) {
+            Some(mut read_bytes) => {
+                read_buffer.append(&mut read_bytes);
+            }
+            None => {
+                println!("{:?}", String::from_utf8(read_buffer).unwrap());
+                std::process::exit(0);
+            }
         }
+    }
 }
